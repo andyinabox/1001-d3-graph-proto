fc819fdb742e69f012c1474fe86cd588fdd1b42f
amdayton <amdayton@gmail.com>
2018-06-09 11:23:01 -0500

    Very basic force graph with text

---
+++ src/js/network-graph.js
@@ -0,0 +1,75 @@
+import * as d3 from 'd3';
+
+export default class NetworkGraph {
+    constructor(id, data) {
+        this.data = data;
+        this.width = 1024;
+        this.height = 768;
+        this.svg = d3.select(id)
+            .append('svg')
+            .attr("width", this.width)
+            .attr("height", this.height);
+        this.simulation = d3.forceSimulation()
+            .force("link", d3.forceLink().id( d => d.id ))
+            .force("charge", d3.forceManyBody())
+            .force("center", d3.forceCenter(this.width / 2, this.height / 2));
+
+    }
+    draw() {
+        let link = this.svg.append("g")
+            .attr("class", "links")
+            .selectAll("line")
+            .data(this.data.links)
+            .enter().append("line");
+
+        let node = this.svg.append("g")
+            .attr("class", "nodes")
+            .selectAll("text")
+            .data(this.data.nodes)
+            .enter().append("text")
+            .attr("class", "node")
+            .text(function (d) { return d.content; })
+            .call(d3.drag()
+                .on("start", this.dragstarted.bind(this))
+                .on("drag", this.dragged.bind(this))
+                .on("end", this.dragended.bind(this)));
+
+        this.simulation
+            .nodes(this.data.nodes)
+            .on("tick", ticked);
+
+        this.simulation.force("link")
+            .links(this.data.links);
+
+        function ticked() {
+            link
+                .attr("x1", function (d) { return d.source.x; })
+                .attr("y1", function (d) { return d.source.y; })
+                .attr("x2", function (d) { return d.target.x; })
+                .attr("y2", function (d) { return d.target.y; });
+
+            node
+                .attr("x", function (d) { return d.x; })
+                .attr("y", function (d) { return d.y; });
+        }
+    }
+
+dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+}
+
+dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+}
+
+
+}
